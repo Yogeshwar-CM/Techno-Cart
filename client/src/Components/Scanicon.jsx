80e8b3ec68261a3037831f509061f8bff3e79172
jeremyito07 <nagwalanshu@gmail.com>
2023-12-07 16:02:57 +0530

    [feat] added style to scan component

--- client/src/Components/Scanicon.jsx
+++ client/src/Components/Scanicon.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 const Scanicon = () => {
   return (
-    <div>
-      <Link to="/scan">
+    <div className="border-2 bg-green-600 px-4">
+      <Link to="/scan" className="">
         <img
           src={scan}
           alt="Scan"
-          className="fixed right-0 bottom-0 m-8 z-50 w-20 bg-white p-2 rounded-md cursor-pointer"
+          className="fixed right-0 bottom-0 m-8 z-50 w-20 px-4 bg-white p-2 rounded-md cursor-pointer"
         />
       </Link>
     </div>
