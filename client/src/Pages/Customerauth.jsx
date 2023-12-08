d2c59367b5e71b86354db636261ed0b4d573fac0
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-08 11:25:35 +0100

    signup page

--- client/src/Pages/Customerauth.jsx
+++ client/src/Pages/Customerauth.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./customer-auth.css";
@@ -52,43 +53,130 @@ const Customerauth = () => {
   };
 
   return (
-    <div className="Customerauth">
+    // <div className="Customerauth">
+    //   {isSignUp ? (
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="top">
+    //         <h1>New Customer</h1>
+    //       </div>
+    //       <div className="middle">
+    //         <p>FullName:</p>
+    //         <input
+    //           type="text"
+    //           placeholder="Your Name"
+    //           name="FullName"
+    //           value={FullName}
+    //           onChange={(e) => setFullName(e.target.value)}
+    //         />
+    //         <p>UserName:</p>
+    //         <input
+    //           type="text"
+    //           placeholder="UserName"
+    //           name="userName"
+    //           value={userName}
+    //           onChange={(e) => setUserName(e.target.value)}
+    //         />
+    //         <p>Password:</p>
+    //         <input
+    //           type="password"
+    //           placeholder="Password"
+    //           name="password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="bottom">
+    //         <button type="button" onClick={handleSignUp}>
+    //           Already have an account?
+    //         </button>
+    //         <button type="submit" id="signup">
+    //           SignUp
+    //         </button>
+    //       </div>
+    //     </form>
+    //   ) : (
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="top">
+    //         <h1>Customer Login</h1>
+    //       </div>
+    //       <div className="middle">
+    //         <p>UserName:</p>
+    //         <input
+    //           type="text"
+    //           placeholder="UserName"
+    //           name="userName"
+    //           value={userName}
+    //           onChange={(e) => setUserName(e.target.value)}
+    //         />
+    //         <p>Password:</p>
+    //         <input
+    //           type="password"
+    //           placeholder="Password"
+    //           name="password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="bottom">
+    //         <button onClick={handleSignUp}>Don't have an account?</button>
+    //         <button type="submit" id="login">
+    //           Login
+    //         </button>
+    //       </div>
+    //     </form>
+    //   )}
+    // </div>
+    <div className="font-[Poppins] w-[100vw] h-[100vh] bg-[#e4e4e4] flex items-center justify-center">
       {isSignUp ? (
-        <form onSubmit={handleSubmit}>
-          <div className="top">
-            <h1>New Customer</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white w-[480px] h-[900px] rounded-[5px] box-shadow flex flex-col items-center py-4 px-8"
+        >
+          <div className="mb-[1.2rem]">
+            <h1 className="font-bold text-[2rem] mb-[1.2rem]">New Customer</h1>
           </div>
-          <div className="middle">
-            <p>FullName:</p>
+          <div className="mb-[1.2rem]">
+            <p className="text-[1.25rem]">FullName:</p>
             <input
               type="text"
               placeholder="Your Name"
               name="FullName"
               value={FullName}
+              className="p-[0.75rem] mb-[1rem] input-box-shadow"
               onChange={(e) => setFullName(e.target.value)}
             />
-            <p>UserName:</p>
+            <p className="text-[1.25rem]">UserName:</p>
             <input
               type="text"
               placeholder="UserName"
               name="userName"
               value={userName}
+              className="p-[0.75rem] mb-[1rem] input-box-shadow"
               onChange={(e) => setUserName(e.target.value)}
             />
-            <p>Password:</p>
+            <p className="text-[1.25rem]">Password:</p>
             <input
               type="password"
               placeholder="Password"
               name="password"
               value={password}
+              className="p-[0.75rem] mb-[1rem] input-box-shadow"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <div className="bottom">
-            <button type="button" onClick={handleSignUp}>
+          <div className="flex flex-col">
+            <button
+              type="button"
+              className="text-[1rem] mt-[0.5rem]"
+              onClick={handleSignUp}
+            >
               Already have an account?
             </button>
-            <button type="submit" id="signup">
+            <button
+              type="submit"
+              className="bg-black text-white text-[1.2rem] p-[0.4rem] rounded-[5px] cursor-pointer mt-[0.5rem]"
+              id="signup"
+            >
               SignUp
             </button>
           </div>
