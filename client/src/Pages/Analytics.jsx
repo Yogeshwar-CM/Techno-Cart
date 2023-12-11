6843d78d6823678d10a15f473bf8b2c860b7e8e8
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-11 09:48:48 +0100

    dashboard and users styles

--- client/src/Pages/Analytics.jsx
+++ client/src/Pages/Analytics.jsx
@@ -19,27 +19,52 @@ const AdminDashboard = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-      <AdminSidebar />
-      <div className="max-w-2xl w-full bg-white p-8 rounded shadow-md">
-        <h1 className="text-3xl font-bold mb-6">Checkout History</h1>
+    // <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+    //   <AdminSidebar />
+    //   <div className="max-w-2xl w-full bg-white p-8 rounded shadow-md">
+    //     <h1 className="text-3xl font-bold mb-6">Checkout History</h1>
 
-        {checkoutHistory.length === 0 ? (
-          <p>No checkout history available.</p>
-        ) : (
-          <ul>
-            {checkoutHistory.map((checkout, index) => (
-              <li key={index} className="border-b py-2">
-                <p className="text-gray-600">
-                  Total Price: ${checkout.totalAmount}
-                </p>
-                <p className="text-gray-600">
-                  Checkout Time: {checkout.createdAt}
-                </p>
-              </li>
-            ))}
-          </ul>
-        )}
+    //     {checkoutHistory.length === 0 ? (
+    //       <p>No checkout history available.</p>
+    //     ) : (
+    //       <ul>
+    //         {checkoutHistory.map((checkout, index) => (
+    //           <li key={index} className="border-b py-2">
+    //             <p className="text-gray-600">
+    //               Total Price: ${checkout.totalAmount}
+    //             </p>
+    //             <p className="text-gray-600">
+    //               Checkout Time: {checkout.createdAt}
+    //             </p>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     )}
+    //   </div>
+    // </div>
+    <div className="w-[100vw] h-[100vh] bg-[#f1f1f1]">
+      <AdminSidebar />
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="max-w-2xl w-full bg-white p-8 rounded shadow-md">
+          <h1 className="text-3xl font-bold mb-6">Checkout History</h1>
+
+          {checkoutHistory.length === 0 ? (
+            <p>No checkout history available.</p>
+          ) : (
+            <ul>
+              {checkoutHistory.map((checkout, index) => (
+                <li key={index} className="border-b py-2">
+                  <p className="text-gray-600">
+                    Total Price: ${checkout.totalAmount}
+                  </p>
+                  <p className="text-gray-600">
+                    Checkout Time: {checkout.createdAt}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
--- client/src/Pages/UsersPage.jsx
+++ client/src/Pages/UsersPage.jsx
@@ -18,19 +18,37 @@ const UsersPage = () => {
   }, []);
 
   return (
-    <div className="users-container">
+    // <div className="users-container">
+    //   <AdminSidebar />
+    //   <h2>User List</h2>
+    //   <ul className="user-list">
+    //     {users.map((user) => (
+    //       <li key={user._id} className="user-item">
+    //         <div className="user-details">
+    //           <span className="fullname">{user.fullname}</span>
+    //           <span className="username">@{user.username}</span>
+    //         </div>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
+    <div className="w-[100vw] h-[100vh] bg-[#f1f1f1]">
       <AdminSidebar />
-      <h2>User List</h2>
-      <ul className="user-list">
-        {users.map((user) => (
-          <li key={user._id} className="user-item">
-            <div className="user-details">
-              <span className="fullname">{user.fullname}</span>
-              <span className="username">@{user.username}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="bg-white w-[600px] p-6">
+          <h2 className="text-[24px] mb-[20px] text-[#333]">User List</h2>
+          <ul className="w-full">
+            {users.map((user) => (
+              <li key={user._id} className="w-full mb-2 border-b-2 p-2">
+                <div className="w-full flex items-center justify-between">
+                  <span className="fullname">{user.fullname}</span>
+                  <span className="username">@{user.username}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
