0164506d5188effcb48fef209e67b20ad369d951
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-21 09:20:19 +0100

    clean up

--- client/src/Pages/Scan.jsx
+++ client/src/Pages/Scan.jsx
@@ -1,4 +1,4 @@
-import  React,{ useEffect } from "react";
+import React, { useEffect } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { useNavigate } from "react-router-dom";
 
@@ -28,37 +28,36 @@ const Scan = () => {
             productID: result,
           }),
         });
-        
+
         if (!productResponse.ok) {
           console.error(
             `Failed to fetch product details: ${productResponse.statusText}`
-            );
-            return;
-          }
-          const productData = await productResponse.json();
-          console.log(productData);
-          const { name, price } = productData;
-          console.log(name);
-          
-          const cartResponse = await fetch(`${BaseURL}/cart/update`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: userID,
-              productID: result,
-              name: name,
-              price: price,
-              quantity: 1,
-            }),
-          });
-          console.log(cartResponse.json());
-          navigate("/cart");
+          );
+          return;
+        }
+        const productData = await productResponse.json();
+        console.log(productData);
+        const { name, price } = productData;
+        console.log(name);
+
+        const cartResponse = await fetch(`${BaseURL}/cart/update`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            id: userID,
+            productID: result,
+            name: name,
+            price: price,
+            quantity: 1,
+          }),
+        });
+        console.log(cartResponse.json());
+        navigate("/cart");
 
         if (!cartResponse.ok) {
           console.error(`Failed to update cart: ${cartResponse.statusText}`);
           return;
         }
-
       } catch (error) {
         console.error("Error processing scan:", error.message);
       }
@@ -75,10 +74,15 @@ const Scan = () => {
 
   return (
     <div className=" bg-[#FFF4F4] mx-auto px-4 flex flex-col justify-center items-center h-screen">
-    <div className="bg-[#2c2c2c] rounded-md p-6">
-      <h1 className="text-lg font-semibold text-center md:text-4xl pb-6 text-[#FFF4F4] px-3">QR Code Scanner</h1>
-      <div id="reader" className="rounded-lg border-none cursor-pointer p-2 flex justify-center items-center flex-col h-[300px] bg-[#FFF4F4]"></div>
-    </div>
+      <div className="bg-[#2c2c2c] rounded-md p-6">
+        <h1 className="text-lg font-semibold text-center md:text-4xl pb-6 text-[#FFF4F4] px-3">
+          QR Code Scanner
+        </h1>
+        <div
+          id="reader"
+          className="rounded-lg border-none cursor-pointer p-2 flex justify-center items-center flex-col h-[300px] bg-[#FFF4F4]"
+        ></div>
+      </div>
     </div>
   );
 };
